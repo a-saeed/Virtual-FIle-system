ec552b43df8b088c2e1503b43b5712222e3a4d03
a-saeed <31377230+a-saeed@users.noreply.github.com>
2019-04-04 16:20:20 +0200

    adding ICommands interface

--- IndexedAllocation.java
+++ IndexedAllocation.java
@@ -1,4 +1,34 @@
 package com;
 
-public class IndexedAllocation {
+public class IndexedAllocation implements ICommands{
+
+    @Override
+    public void createFile(int size, String[] path, int pLength) {
+
+    }
+
+    @Override
+    public void createFolder(String[] path, int pLength) {
+
+    }
+
+    @Override
+    public void deleteFile(String[] path, int pLength) {
+
+    }
+
+    @Override
+    public void deleteFolder(String[] path, int pLength) {
+
+    }
+
+    @Override
+    public void displayDiskStatus(Directory root) {
+
+    }
+
+    @Override
+    public void displayDiskStructure(Directory root) {
+
+    }
 }
--- LinkedAllocation.java
+++ LinkedAllocation.java
@@ -1,4 +1,33 @@
 package com;
 
-public class LinkedAllocation {
+public class LinkedAllocation implements  ICommands {
+    @Override
+    public void createFile(int size, String[] path, int pLength) {
+
+    }
+
+    @Override
+    public void createFolder(String[] path, int pLength) {
+
+    }
+
+    @Override
+    public void deleteFile(String[] path, int pLength) {
+
+    }
+
+    @Override
+    public void deleteFolder(String[] path, int pLength) {
+
+    }
+
+    @Override
+    public void displayDiskStatus(Directory root) {
+
+    }
+
+    @Override
+    public void displayDiskStructure(Directory root) {
+
+    }
 }
