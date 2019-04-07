245c852b3f0bf13d9448855df2e47f410a722f36
MohammadAhmadMostafa <Mohammad.rady.98@gmail.com>
2019-04-07 17:13:47 +0200

    adding some logic in Linked class

--- File.java
+++ File.java
@@ -2,9 +2,14 @@ package com;
 
 import java.util.ArrayList;
 
-public class File {
+public class File
+{
     public String fName;
     public ArrayList<Integer> allocatedBlocks;
     public  int size;
+    File()
+    {
+        allocatedBlocks = new ArrayList<>();
+    }
 
 }
